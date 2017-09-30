179373e7831626e1f2e7c3b837c507147b3dc8ab
Violeta Sosa <vsosa.ud@gmail.com>
2017-09-29 22:19:03 -0500

    Primer commit

---
+++ JS/global.js
@@ -0,0 +1,261 @@
+/*
+ * jQuery v1.9.1 included
+ */
+
+$(document).ready(function() {
+
+  var txt =document.getElementsByClassName('texto');
+  for (var i = 0, len = txt.length; i < len; i++) {
+   var texto=txt[i].innerHTML;
+   var rex = /(<([^>]+)>)/ig;
+   var contenido=  texto.replace(rex , "");
+   txt[i].innerHTML=(contenido.substring(0,150)).concat("...");
+
+}
+
+
+   var catItems=document.getElementsByClassName('catalogoItem');
+
+    for (var i = 0, len = catItems.length; i < len; i++) {
+
+        var string ='//p4.zdassets.com/hc/theme_assets/2025662/115000009683/'+(catItems[i].id).replace(new RegExp(' ', 'g'), '_')+'.png';
+   catItems[i].style.backgroundImage = "url('" + string + "')";
+catItems[i].src = string;
+
+}
+
+
+
+  // social share popups
+  $(".share a").click(function(e) {
+    e.preventDefault();
+    window.open(this.href, "", "height = 500, width = 500");
+  });
+
+  // show form controls when the textarea receives focus or backbutton is used and value exists
+  var $commentContainerTextarea = $(".comment-container textarea"),
+  $commentContainerFormControls = $(".comment-form-controls, .comment-ccs");
+
+  $commentContainerTextarea.one("focus", function() {
+    $commentContainerFormControls.show();
+  });
+
+  if ($commentContainerTextarea.val() !== "") {
+    $commentContainerFormControls.show();
+  }
+
+  // Expand Request comment form when Add to conversation is clicked
+  var $showRequestCommentContainerTrigger = $(".request-container .comment-container .comment-show-container"),
+    $requestCommentFields = $(".request-container .comment-container .comment-fields"),
+    $requestCommentSubmit = $(".request-container .comment-container .request-submit-comment");
+
+  $showRequestCommentContainerTrigger.on("click", function() {
+    $showRequestCommentContainerTrigger.hide();
+    $requestCommentFields.show();
+    $requestCommentSubmit.show();
+    $commentContainerTextarea.focus();
+  });
+
+  // Mark as solved button
+  var $requestMarkAsSolvedButton = $(".request-container .mark-as-solved:not([data-disabled])"),
+    $requestMarkAsSolvedCheckbox = $(".request-container .comment-container input[type=checkbox]"),
+    $requestCommentSubmitButton = $(".request-container .comment-container input[type=submit]");
+
+  $requestMarkAsSolvedButton.on("click", function () {
+    $requestMarkAsSolvedCheckbox.attr("checked", true);
+    $requestCommentSubmitButton.prop("disabled", true);
+    $(this).attr("data-disabled", true).closest("form").submit();
+  });
+
+  // Change Mark as solved text according to whether comment is filled
+  var $requestCommentTextarea = $(".request-container .comment-container textarea");
+
+  $requestCommentTextarea.on("keyup", function() {
+    if ($requestCommentTextarea.val() !== "") {
+      $requestMarkAsSolvedButton.text($requestMarkAsSolvedButton.data("solve-and-submit-translation"));
+      $requestCommentSubmitButton.prop("disabled", false);
+    } else {
+      $requestMarkAsSolvedButton.text($requestMarkAsSolvedButton.data("solve-translation"));
+      $requestCommentSubmitButton.prop("disabled", true);
+    }
+  });
+
+  // Disable submit button if textarea is empty
+  if ($requestCommentTextarea.val() === "") {
+    $requestCommentSubmitButton.prop("disabled", true);
+  }
+
+  // Submit requests filter form in the request list page
+  $("#request-status-select, #request-organization-select")
+    .on("change", function() {
+      search();
+    });
+
+  // Submit requests filter form in the request list page
+  $("#quick-search").on("keypress", function(e) {
+    if (e.which === 13) {
+      search();
+    }
+  });
+
+  function search() {
+    window.location.search = $.param({
+      query: $("#quick-search").val(),
+      status: $("#request-status-select").val(),
+      organization_id: $("#request-organization-select").val()
+    });
+  }
+
+  $(".header .icon-menu").on("click", function(e) {
+    e.stopPropagation();
+    var menu = document.getElementById("user-nav");
+    var isExpanded = menu.getAttribute("aria-expanded") === "true";
+    menu.setAttribute("aria-expanded", !isExpanded);
+  });
+
+  if ($("#user-nav").children().length === 0) {
+    $(".header .icon-menu").hide();
+  }
+
+  // Submit organization form in the request page
+  $("#request-organization select").on("change", function() {
+    this.form.submit();
+  });
+
+  // Toggles expanded aria to collapsible elements
+  $(".collapsible-nav, .collapsible-sidebar").on("click", function(e) {
+    e.stopPropagation();
+    var isExpanded = this.getAttribute("aria-expanded") === "true";
+    this.setAttribute("aria-expanded", !isExpanded);
+  });
+
+
+  /**Limitar el número de publicaciones destacadas**/
+if( document.location.href.indexOf('section') == -1 ) {
+var categories = $('ul.article-list');
+for (var j = categories.length - 1; j >= 0; j--) {
+var articles = $(categories[j]).find('li'),
+nativeMore = $(categories[j]).siblings('.see-all-articles');
+if ( articles.length > 6 ) {
+for (var k = 6; k < articles.length; k++ ) {
+$(articles[k]).hide();
+}
+var moreLink = $(categories[j]).parent().find('h3 a').attr('href'),
+linkText = '';
+if( articles.length <= 6 && nativeMore.length == 0 ) {
+linkText += "See all " + articles.length + " articles";
+}
+
+}
+}
+}
+
+
+  //slideshow style interval
+var autoSwap = setInterval( swap,3500);
+
+//pause slideshow and reinstantiate on mouseout
+$('ul, span').hover(
+  function () {
+    clearInterval(autoSwap);
+},
+  function () {
+   autoSwap = setInterval( swap,3500);
+});
+
+//global variables
+var items = [];
+var startItem = 1;
+var position = 0;
+var itemCount = $('.carousel li.itemsC').length;
+var leftpos = itemCount;
+var resetCount = itemCount;
+
+//unused: gather text inside items class
+$('li.items').each(function(index) {
+    items[index] = $(this).text();
+});
+
+//swap images function
+function swap(action) {
+  var direction = action;
+
+  //moving carousel backwards
+  if(direction == 'counter-clockwise') {
+    var leftitem = $('.left-pos').attr('id') - 1;
+    if(leftitem == 0) {
+      leftitem = itemCount;
+    }
+
+    $('.right-pos').removeClass('right-pos').addClass('back-pos');
+    $('.main-pos').removeClass('main-pos').addClass('right-pos');
+    $('.left-pos').removeClass('left-pos').addClass('main-pos');
+    $('#'+leftitem+'').removeClass('back-pos').addClass('left-pos');
+
+    startItem--;
+    if(startItem < 1) {
+      startItem = itemCount;
+    }
+  }
+
+  //moving carousel forward
+  if(direction == 'clockwise' || direction == '' || direction == null ) {
+    function pos(positionvalue) {
+      if(positionvalue != 'leftposition') {
+        //increment image list id
+        position++;
+
+        //if final result is greater than image count, reset position.
+        if((startItem+position) > resetCount) {
+          position = 1-startItem;
+        }
+      }
+
+      //setting the left positioned item
+      if(positionvalue == 'leftposition') {
+        //left positioned image should always be one left than main positioned image.
+        position = startItem - 1;
+
+        //reset last image in list to left position if first image is in main position
+        if(position < 1) {
+          position = itemCount;
+        }
+      }
+
+      return position;
+    }
+
+   $('#'+ startItem +'').removeClass('main-pos').addClass('left-pos');
+   $('#'+ (startItem+pos()) +'').removeClass('right-pos').addClass('main-pos');
+   $('#'+ (startItem+pos()) +'').removeClass('back-pos').addClass('right-pos');
+   $('#'+ pos('leftposition') +'').removeClass('left-pos').addClass('back-pos');
+
+    startItem++;
+    position=0;
+    if(startItem > itemCount) {
+      startItem = 1;
+    }
+  }
+}
+
+//next button click function
+$('#next').click(function() {
+  swap('clockwise');
+});
+
+//prev button click function
+$('#prev').click(function() {
+  swap('counter-clockwise');
+});
+
+//if any visible items are clicked
+$('li').click(function() {
+  if($(this).attr('class') == 'items left-pos') {
+     swap('counter-clockwise');
+  }
+  else {
+    swap('clockwise');
+  }
+});
+
+   });
